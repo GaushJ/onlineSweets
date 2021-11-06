429df5df2cc3e653c3755a5dffac1a9ed082c881
Gauransh <gauranshjuneja@gmail.com>
2021-11-06 21:47:46 +0530

    added server and database

---
+++ Client/src/Components/Images/Images.jsx
@@ -0,0 +1,8 @@
+const IMAGES = {
+    
+    Image1:require('./image1.jpg').default,
+    Image2:require('./image2.jpg').default,
+    Image3:require('./image3.jpg').default,
+    Image4:require('./image4.jpg').default,
+}
+export default IMAGES;
